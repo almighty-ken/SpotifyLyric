817aab1dc0fac115a9c174ef0f0a56000494c422
kencheng <kenny851017@gmail.com>
2017-02-02 09:58:18 -0500

    updated request header

--- show_lyric.py
+++ show_lyric.py
@@ -35,8 +35,16 @@ def fetch_time():
     return time
 
 def get_lyric_from_link(link):
+    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+           'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+           'Accept-Encoding': 'none',
+           'Accept-Language': 'en-US,en;q=0.8',
+           'Connection': 'keep-alive'}
+    req = urllib2.Request(link, headers=hdr)
     try:
-        data = urllib2.urlopen(link)
+        site = urllib2.urlopen(req)
+        data = site.read()
     except:
         return 'Sorry no lyrics!'
     soup = BeautifulSoup(data,'html.parser')
